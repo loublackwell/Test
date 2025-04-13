d7c60141163c68decad5fdab693d60cc469def61
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-13 14:16:52 -0400

    another commit

--- my_app.py
+++ my_app.py
@@ -1,9 +1,6 @@
 import os
 import subprocess
 import json
-from google import genai
-from tika import parser
-from tika import initVM, parser
 # Explicitly initialize Java VM first
 initVM()
 import chromadb
