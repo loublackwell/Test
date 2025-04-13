20f290056be2bc7dc0e9749db5c44576afa9e6ac
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-13 14:07:39 -0400

    another commit

--- my_app.py
+++ my_app.py
@@ -1,3 +1,14 @@
+import os
+import subprocess
+import json
+from google import genai
+from tika import parser
+from tika import initVM, parser
+# Explicitly initialize Java VM first
+initVM()
+import chromadb
+from sentence_transformers import SentenceTransformer
+
 import streamlit as st
 st.sidebar.title("RAG-Demo")
 
