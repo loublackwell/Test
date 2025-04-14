2a23db1384c3cca67637a719254962d6f1fbabb9
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-14 18:43:46 -0400

    Another Update

--- my_app.py
+++ my_app.py
@@ -268,7 +268,7 @@ def parse_query(out,verse_dict):
                         pydict=middle_block[json_start:json_end+1]
                         #pydict=pydict1.replace('\\','\\\\')
                         st.text(f"PARSE:{pydict}")
-                       print(pydict)
+                        print(pydict)
 
                         
     return dict_block,answers,report_dict,error
