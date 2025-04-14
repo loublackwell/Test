022a29931096c731e716b426d44bf85bd7565344
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-14 14:42:07 -0400

    Track large JAR file with Git LFS

--- my_app.py
+++ my_app.py
@@ -306,5 +306,7 @@ if question!="":
         
         #Generate Conclusion/Summary given the answers
         out=query_gemini(task2)
+"""
         llm_dict2,answers2,report_dict2,error=parse_query(out,verse_dict)#HANDLE PARSE ERROR
         st.write(llm_dict2)
+"""
