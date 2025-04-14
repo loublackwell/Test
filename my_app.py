77fee0a14a35ef22064c215ee8f27eca90036632
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-14 18:45:16 -0400

    Another Update

--- my_app.py
+++ my_app.py
@@ -267,7 +267,7 @@ def parse_query(out,verse_dict):
                     if json_end>-1:
                         pydict=middle_block[json_start:json_end+1]
                         #pydict=pydict1.replace('\\','\\\\')
-                        st.text(f"PARSE:{pydict}")
+                        st.text(f"PARSE1:{pydict}")
                         print(pydict)
 
                         
