8a6fea53d8bc46c08034817c2b9977b49f426c56
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-14 17:38:36 -0400

    Another Update

--- my_app.py
+++ my_app.py
@@ -94,7 +94,7 @@ def query_texts(query_text, top_k):
     for i in indices[0]:
         ID=metadata_store[i][0]#ID
         doc1=metadata_store[i][1].strip().replace("\n","")#Document
-        doc=doc1.replace('"',"'")                      
+        doc=doc1.replace('"',""'")                      
         metadata=metadata_store[i][2]
         ids.append(ID)
         documents.append(doc)
