9ba9f665420bc26684433c9b02977ca11e0db88f
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-13 20:40:44 -0400

    Update

--- my_app.py
+++ my_app.py
@@ -5,7 +5,14 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
-#from google import genai
+from google import genai
+
+#Declaration of variables
+global my_key
+cwd=os.getcwd()#Current working Directory
+tika_jar_path=os.path.join("tika_jar_file","tika-app-2.9.3.jar")#Relative path to tika jar file
+file_path = os.path.join(cwd,"en_GC.pdf")#Path to PDF file
+
 
 st.sidebar.title("RAG-Demo")
 
